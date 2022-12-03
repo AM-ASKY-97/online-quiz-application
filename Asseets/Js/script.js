cfa0030dea630bb2c767525e8193c781f3085abe
AM-ASKY <am.asky97@gmail.com>
2022-12-03 12:04:57 +0530

    I have changed the logo on the home page

--- Asseets/Js/script.js
+++ Asseets/Js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
+    validate();
+});
+
+function validate() {
     $("#userName").click(function () {
         var name = $("#name").val();
 
         if (name == "") {
             $("#errorName").text("Please enter your name");
+            $('#name').css('border', 'solid 1px red');
         }
 
         else {
@@ -11,4 +16,9 @@ $(document).ready(function () {
             window.location.href = "courses.html";
         }
     });
-});
+}
+
+function myInput() {
+    $("#errorName").text(" ");
+    $('#name').css('border', 'solid 1px green');
+}
