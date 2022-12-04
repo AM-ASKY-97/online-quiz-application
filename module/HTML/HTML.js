739b47a6edbd318f35cf1b360d18284577c524cf
AM-ASKY <am.asky97@gmail.com>
2022-12-05 00:23:44 +0530

    score id Name Changed

--- module/HTML/HTML.js
+++ module/HTML/HTML.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     $('#quiz-2, #quiz-3, #quiz-4, #scor').hide();
 
     quiz1();
@@ -10,24 +10,23 @@ $(document).ready(function(){
 
 var count = 0;
 
-function quiz1()
-{
+function quiz1() {
     $('#ans-btn-1').prop('disabled', true);
 
-    $('#ans-1-1').on('change', function(){
+    $('#ans-1-1').on('change', function () {
         $('#ans-btn-1').prop('disabled', false);
     });
 
-    $('#ans-1-2').on('change', function(){
+    $('#ans-1-2').on('change', function () {
         $('#ans-btn-1').prop('disabled', false);
     });
 
-    $('#ans-1-3').on('change', function(){
+    $('#ans-1-3').on('change', function () {
         $('#ans-btn-1').prop('disabled', false);
     });
 
-    $('#ans-btn-1').on('click', function(){
-        
+    $('#ans-btn-1').on('click', function () {
+
         if ($("#ans-1-1").is(":checked")) {
             ++count;
         }
@@ -37,27 +36,26 @@ function quiz1()
     });
 }
 
-function quiz2()
-{
+function quiz2() {
     $('#ans-btn-2').prop('disabled', true);
 
-    $('#ans-2-1').on('change', function(){
+    $('#ans-2-1').on('change', function () {
         $('#ans-btn-2').prop('disabled', false);
     });
 
-    $('#ans-2-2').on('change', function(){
+    $('#ans-2-2').on('change', function () {
         $('#ans-btn-2').prop('disabled', false);
     });
 
-    $('#ans-2-3').on('change', function(){
+    $('#ans-2-3').on('change', function () {
         $('#ans-btn-2').prop('disabled', false);
     });
 
-    $('#ans-2-4').on('change', function(){
+    $('#ans-2-4').on('change', function () {
         $('#ans-btn-4').prop('disabled', false);
     });
 
-    $('#ans-btn-2').on('click', function(){
+    $('#ans-btn-2').on('click', function () {
 
         if ($("#ans-2-3").is(":checked")) {
             ++count;
@@ -68,27 +66,26 @@ function quiz2()
     });
 }
 
-function quiz3()
-{
+function quiz3() {
     $('#ans-btn-3').prop('disabled', true);
 
-    $('#ans-3-1').on('change', function(){
+    $('#ans-3-1').on('change', function () {
         $('#ans-btn-3').prop('disabled', false);
     });
 
-    $('#ans-3-2').on('change', function(){
+    $('#ans-3-2').on('change', function () {
         $('#ans-btn-3').prop('disabled', false);
     });
 
-    $('#ans-3-3').on('change', function(){
+    $('#ans-3-3').on('change', function () {
         $('#ans-btn-3').prop('disabled', false);
     });
 
-    $('#ans-3-4').on('change', function(){
+    $('#ans-3-4').on('change', function () {
         $('#ans-btn-3').prop('disabled', false);
     });
 
-    $('#ans-btn-3').on('click', function(){
+    $('#ans-btn-3').on('click', function () {
 
         if ($("#ans-3-2").is(":checked")) {
             ++count;
@@ -99,23 +96,22 @@ function quiz3()
     });
 }
 
-function quiz4()
-{
+function quiz4() {
     $('#ans-btn-4').prop('disabled', true);
 
-    $('#ans-4-1').on('change', function(){
+    $('#ans-4-1').on('change', function () {
         $('#ans-btn-4').prop('disabled', false);
     });
 
-    $('#ans-4-2').on('change', function(){
+    $('#ans-4-2').on('change', function () {
         $('#ans-btn-4').prop('disabled', false);
     });
 
-    $('#ans-4-3').on('change', function(){
+    $('#ans-4-3').on('change', function () {
         $('#ans-btn-4').prop('disabled', false);
     });
 
-    $('#ans-btn-4').on('click', function(){
+    $('#ans-btn-4').on('click', function () {
 
         if ($("#ans-4-3").is(":checked")) {
             ++count;
@@ -124,6 +120,6 @@ function quiz4()
         document.getElementById('score').innerHTML = count;
 
         $('#quiz-4').hide();
-        $('#sco').show();
+        $('#scor').show();
     });
 }
